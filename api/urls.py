7bbe8a5d14e5c52eb123a3280d9a0f814f868a43
unknown <ds@ESN501D1278469.educ501.local>
2025-08-21 16:41:30 -0300

    2atuali

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('autores', AutorView.as_view()),
     path('authors', list_autor),
     path('editoras', EditoraView.as_view()),
-    path('livros', LivroView.as_view())
+    path('livros', LivroView.as_view()),
+
+    path('autor/<int:pk>', AutoresDetailView.as_view()),
+    path('editora/<int:pk>', AutoresDetailView.as_view()),
+    path('livro/<int:pk>', LivroDetailView.as_view())
 ]
