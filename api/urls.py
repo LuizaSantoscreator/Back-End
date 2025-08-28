c9eb27051fb61db4bb23f15899ecd34732c5cee3
LuizaSantoscreator <lusantos1017@gmail.com>
2025-08-28 16:40:37 -0300

    sua mensagem

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('authors', list_autor),
     path('editoras', EditoraView.as_view()),
     path('livros', LivroView.as_view()),
+    path('buscar/', AutorView.as_view()),
 
     path('autor/<int:pk>', AutoresDetailView.as_view()),
     path('editora/<int:pk>', AutoresDetailView.as_view()),
--- api/views.py
+++ api/views.py
@@ -6,11 +6,18 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
 
 class AutorView(ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['id']
+    search_nome = ['nome']
+
 
 class AutoresDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
